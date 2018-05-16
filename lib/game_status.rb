4b143bd83d0ba77459738503096fda12c9766574
JohnMark <johnmarkludwick@gmail.com>
2018-05-16 16:22:35 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -88,9 +88,9 @@ end
 
 def winner(board)
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return
+    return "X"
   elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-    return
+    return "O"
   else
     false
   end
